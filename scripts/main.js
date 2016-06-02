f4489b3d0c252b885c3ef15b1ac7696553a95c70
Kevin LeBaron <kevaleb@gmail.com>
2016-06-02 10:34:55 -0600

    tracking

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,10 @@ $(function() {
 		    $("body > header > div > nav").toggleClass("visible");
 		});
 		
-//		mixpanel.track('Viewed', {
-//	//	    'page name' : document.title,
-//		    'url' : window.location.pathname
-//		});
+		mixpanel.track('Viewed', {
+	//	    'page name' : document.title,
+		    'url' : window.location.pathname
+		});
 		
 		
 	});
